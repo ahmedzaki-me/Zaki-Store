93f522515dfa4b3ef59eb4f02593eb4dff7b50da
Ahmed <ahmedzaki.developer@gmail.com>
2026-05-11 16:45:13 +0300

    fix cache tags 555

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import HeroSection from "@/components/HeroSection";
 import SecondSection from "@/components/SecondSection";
 import HowToUseSection from "@/components/HowToUseSection";
 import Menu from "@/components/Menu";
-import RealtimeRefresher from "@/hooks/RealtimeRefresher";
+import { CacheRevalidationProvider } from "@/components/cache-revalidation-provider";
 
 export default async function Home({
   searchParams,
 }: {
   searchParams: Promise<{ category?: string }>;
 }) {
-  RealtimeRefresher();
   return (
     <>
+      <CacheRevalidationProvider />
       <HeroSection />
       <SecondSection />
       <HowToUseSection />
---
+++ components/cache-revalidation-provider.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import RealtimeRefresher from "@/hooks/RealtimeRefresher";
+
+export function CacheRevalidationProvider() {
+  RealtimeRefresher();
+  return null;
+}
